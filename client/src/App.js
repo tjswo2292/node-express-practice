e08517f15a23a33d6207d1a8d18e0fce34e2fc3f
seonjae <tjswo2292@naver.com>
2022-12-31 01:25:12 +0900

    12/30

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [test, setTest] = useState("");
+
+	useEffect(() => {
+		let body = {
+			text: "hello",
+		};
+
+		axios
+			.post("/api/test", body)
+			.then((res) => {
+				setTest(res.data.text);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
+	return <h1>{test}</h1>;
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,43 @@
 const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
 const app = express();
 const port = 5000;
 
+// mongodb+srv://tjswo2292:20180411zZ!@cluster0.anbkxn8.mongodb.net/?retryWrites=true&w=majority
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+// 밑의 2가지 구문을 추가 해주면 client에서 보내는 데이터인 body를 파싱한다.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const { Post } = require("./Model/Post.js");
+
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+	mongoose
+		.connect(
+			"mongodb+srv://tjswo2292:20180411zZ!@cluster0.anbkxn8.mongodb.net/Community?retryWrites=true&w=majority"
+		)
+		.then(() => {
+			console.log(`Example app listening on port ${port}`);
+			console.log("Connecting MongoDB...");
+		})
+		.catch((err) => {
+			console.log(`${err}`);
+		});
 });
 
 app.get("/", (req, res) => {
-	res.send("Hello World!");
+	res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+app.post("/api/test", (req, res) => {
+	const CommunityPost = new Post({ title: "제목", content: "글" });
+	CommunityPost.save().then(() => {
+		res.status(200).json({ success: true, text: "hello my name is seonjae" });
+	});
 });
